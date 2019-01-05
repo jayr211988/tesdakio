e1a9fba6a67562be7a813f1226c3a88e87f7717a
jayr211988 <rogeliomendeja@gmail.com>
2019-01-05 21:54:37 +0800

    my new

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
-var http = require('http');
+// var http = require('http');
 
-function onRequest(request,response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'})
-    response.write('Hello World');
-    response.end();
-}
+// function onRequest(request,response) {
+//     response.writeHead(200, {'Content-Type': 'text/plain'})
+//     response.write('Hello World');
+//     response.end();
+// }
 
-http.createServer(onRequest).listen(8080);
+// http.createServer(onRequest).listen(8080);
+
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.get('/', function(request,response){
+  response.sendFile(path.join(__dirname,'index.html'))  
+})
+
+app.listen(8080)
